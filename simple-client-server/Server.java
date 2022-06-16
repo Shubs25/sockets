f84b2758617962b335bc63fe24c76927237d8a6f
Shubham <79351060+Shubs25@users.noreply.github.com>
2022-06-16 23:59:39 +0530

    Create Server.java

---
+++ simple-client-server/Server.java
@@ -0,0 +1,20 @@
+package simple-client-server;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+
+public class Server {
+    public static void main(String[] args) throws Exception {
+        ServerSocket ss = new ServerSocket(6969);
+        Socket s = ss.accept();
+
+        DataInputStream dis = new DataInputStream(s.getInputStream());
+        System.out.println(dis.readUTF());
+
+        dis.close();
+        s.close();
+        ss.close();
+
+    }
+}
